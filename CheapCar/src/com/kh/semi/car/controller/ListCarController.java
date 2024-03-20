d41bed52e021264f7d86160a50847bf47fe25f14
PhilnoS2 <164119126+PhilnoS2@users.noreply.github.com>
2024-03-20 18:15:07 +0900

    현빈 - 컨트롤러

---
+++ CheapCar/src/com/kh/semi/car/controller/ListCarController.java
@@ -0,0 +1,63 @@
+package com.kh.semi.car.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.semi.car.model.service.CarService;
+import com.kh.semi.car.model.vo.Car;
+
+/**
+ * Servlet implementation class ListCarController
+ */
+@WebServlet("/list.car")
+public class ListCarController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListCarController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		/*
+		int listCount; // 현재 게시판 글 총 개수
+		int currentPage; // 현재 페이지
+		int pageLimit; // 페이지 하단에 보여질 페이징 바 개수
+		int boardLimit; // 한페이지에 보여질 최대 개수
+		
+		int maxPage; // 가장 마지막 페이지가 몇 번 페이지인지
+		int startPage; // 페이지 하단에 보여줄 페이징바의 시작수
+		int endPage; // 페이지 하단에 보여질 페이징바의 끝 수
+		
+		new CarService().selectListCount();
+		*/
+		
+		ArrayList<Car> carList =new CarService().selectCarList();
+		
+		request.setAttribute("carList", carList);
+		
+		request.getRequestDispatcher("views/car/carList.jsp").forward(request, response);		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
