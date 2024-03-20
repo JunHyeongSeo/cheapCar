3645b3418f882889b1c1fc0f2cb164f5a35c31d4
Beenzinoooooooooo <306kjung@naver.com>
2024-03-20 18:29:43 +0900

    ㅁ
    ㅁ

--- CheapCar/src/com/kh/semi/admin/admin_1/MemberListController.java
+++ CheapCar/src/com/kh/semi/admin/admin_1/MemberListController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.kh.semi.common.model.vo.PageInfo;
+import com.kh.semi.member.model.service.MemberService;
 
 /**
  * Servlet implementation class MemberListController
@@ -40,78 +41,20 @@ public class MemberListController extends HttpServlet {
 				int startPage;
 				int endPage;
 				
-				// * listCount : 총 게시글의 수
-				listCount = new BoardService().selectListCount();
+				listCount = new MemberService().selectListCount();
 				
-				// * currentPage : 현재 페이지(사용자가 요청한 페이지)
 				currentPage = Integer.parseInt(request.getParameter("currentPage"));
 				
-				// * pageLimit : 페이징바 최대 개수
 				pageLimit = 10;
 				
-				// * boardLimit : 한 페이지에 보여질 게시글의 최대 개수
-				boardLimit = 10;
+				boardLimit = 5;
 				
-				// * maxPage : 가장 마지막페이지가 몇번 페이지인지(총 페이지 개수)
-				/*
-				 * listCount, boardLimit에 영향을 받음
-				 * 
-				 * - 공식 구하기
-				 * 단, boardLimit이 10이라는 가정 하에 규칙을 찾아보자
-				 * 
-				 * 총 개수(listCount)	/	게시글 개수(boardLimit)		== maxPage(마지막페이지)
-				 * 100				/	10						== 10.0	
-				 * 107				/	10						== 10.7
-				 * 
-				 * => 나눗셈 결과에 소수점을 붙여서 올림처리를 할 경우 maxPage가 됨
-				 * 
-				 * 스텝
-				 * 1. listCount를 double로 변환.
-				 * 2. listCount / boardLimit
-				 * 3. Math.ceil() => 결과를 올림처리
-				 * 4. 결과값을 int로 형변환
-				 * 
-				 */
 				maxPage = (int)Math.ceil((double)listCount / boardLimit);
 				
-				// * startPage : 페이지하단에 보여질 페이징바의 시작 수
-				/*
-				 * currentPage, pageLimit에 영향을 받음
-				 * 
-				 * - 공식
-				 * 단, pageLimit이 10이라고 가정
-				 * 
-				 * - startPage : 1,11,21,31,,,,,,, => n * 10 + 1
-				 * 
-				 * 만약에 pageLimit이 5였다면
-				 * - startPage : 1,6,11,16,21,26,,, => n * 5 + 1
-				 * 
-				 * 즉, startPage = n * pageLimit + 1;
-				 * 
-				 * currentPage = 1
-				 * 
-				 * startPage = ((currentpage-1) / pageLimit) * 
-				 * 
-				 */
 				startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
 				
-				// * endPage : 페이지 하단에 보여질 페이징바의 끝 수
-				/*
-				 * stargPage, pageLimit에 영향을 받음(단, maxPage도 마지막 페이징 바에 대해 영향을 끼침)
-				 * 
-				 * - 공식
-				 * 	  단, pageLimit이 10 이라고 가정
-				 * 
-				 * startPage : 1  => endPage : 10
-				 * startPage : 11 => endPage : 20
-				 * startPage : 21 => endPage : 30
-				 * 
-				 */
 				endPage = startPage + pageLimit - 1;
 				
-				// 예시) startPage가 31이라서 endPage에는 40이 들어갔는데
-				// maxPage가 35다?
-				// endPage를 maxPage값으로 변경
 				if(endPage > maxPage) {
 					endPage = maxPage;
 				}
