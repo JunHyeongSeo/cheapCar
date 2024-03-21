9054992e950f15c7f599351e49fca72b568ecc57
dongja86 <becksmania7@gmail.com>
2024-03-21 15:17:19 +0900

    동욱

--- CheapCar/src/com/kh/semi/notice/model/vo/Notice.java
+++ CheapCar/src/com/kh/semi/notice/model/vo/Notice.java
@@ -1,13 +1,9 @@
 package com.kh.semi.notice.model.vo;
 
+import java.sql.Date;
+
 public class Notice {
-	NOTICE_NO
-	NOTICE_TITLE
-	NOTICE_CONTENT
-	CREATE_DATE
-	STATUS
-	COUNT
-	MEMBER_NO
+
 	private int noticeNo;
 	private String noticeTitle;
 	private String noticeContent;
@@ -16,6 +12,143 @@ public class Notice {
 	private int count;
 	private int memberNo;
 	
+	public Notice() {
+		super();
+	}
+
+	public Notice(int noticeNo, String noticeTitle, String noticeContent, Date createDate, String status, int count,
+			int memberNo) {
+		super();
+		this.noticeNo = noticeNo;
+		this.noticeTitle = noticeTitle;
+		this.noticeContent = noticeContent;
+		this.createDate = createDate;
+		this.status = status;
+		this.count = count;
+		this.memberNo = memberNo;
+	}
+
+	public int getNoticeNo() {
+		return noticeNo;
+	}
+
+	public void setNoticeNo(int noticeNo) {
+		this.noticeNo = noticeNo;
+	}
+
+	public String getNoticeTitle() {
+		return noticeTitle;
+	}
+
+	public void setNoticeTitle(String noticeTitle) {
+		this.noticeTitle = noticeTitle;
+	}
+
+	public String getNoticeContent() {
+		return noticeContent;
+	}
+
+	public void setNoticeContent(String noticeContent) {
+		this.noticeContent = noticeContent;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getMemberNo() {
+		return memberNo;
+	}
+
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + count;
+		result = prime * result + ((createDate == null) ? 0 : createDate.hashCode());
+		result = prime * result + memberNo;
+		result = prime * result + ((noticeContent == null) ? 0 : noticeContent.hashCode());
+		result = prime * result + noticeNo;
+		result = prime * result + ((noticeTitle == null) ? 0 : noticeTitle.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Notice other = (Notice) obj;
+		if (count != other.count)
+			return false;
+		if (createDate == null) {
+			if (other.createDate != null)
+				return false;
+		} else if (!createDate.equals(other.createDate))
+			return false;
+		if (memberNo != other.memberNo)
+			return false;
+		if (noticeContent == null) {
+			if (other.noticeContent != null)
+				return false;
+		} else if (!noticeContent.equals(other.noticeContent))
+			return false;
+		if (noticeNo != other.noticeNo)
+			return false;
+		if (noticeTitle == null) {
+			if (other.noticeTitle != null)
+				return false;
+		} else if (!noticeTitle.equals(other.noticeTitle))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Notice [noticeNo=" + noticeNo + ", noticeTitle=" + noticeTitle + ", noticeContent=" + noticeContent
+				+ ", createDate=" + createDate + ", status=" + status + ", count=" + count + ", memberNo=" + memberNo
+				+ "]";
+	}
 	
 	
-}
+	
+	
+	
+	
+	
+	
+	
+}//
