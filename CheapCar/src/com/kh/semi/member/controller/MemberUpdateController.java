8e5e25a2deaddc3e4b1a06db5207cfd706b86186
SeoJunHyeong <110711429+JunHyeongSeo@users.noreply.github.com>
2024-03-21 13:53:20 +0900

    Update MemberUpdateController.java

--- CheapCar/src/com/kh/semi/member/controller/MemberUpdateController.java
+++ CheapCar/src/com/kh/semi/member/controller/MemberUpdateController.java
@@ -1,11 +1,15 @@
 package com.kh.semi.member.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.semi.member.model.service.MemberService;
+import com.kh.semi.member.model.vo.Member;
 
 /**
  * Servlet implementation class MemberUpdateController
@@ -26,7 +30,23 @@ public class MemberUpdateController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		
+		String memberId = request.getParameter("memberId");
+		String memberName = request.getParameter("memberName");
+		String birthday = request.getParameter("birthday");
+		String phone = request.getParameter("phone");
+		String email = request.getParameter("email");
+		
+		Member member = new Member();
+		
+		member.setMemberId(memberId);
+		member.setMemberName(memberName);
+		member.setBirthday(birthday);
+		member.setPhone(phone);
+		member.setEmail(email);
+		
+		new MemberService().updatemember(member);
+		
 		
 	
 	}
