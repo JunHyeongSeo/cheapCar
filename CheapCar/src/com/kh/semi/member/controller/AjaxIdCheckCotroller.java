983202f9114763649a33849d51e56f5ce5439b68
SeoJunHyeong <110711429+JunHyeongSeo@users.noreply.github.com>
2024-03-21 18:28:30 +0900

    12
    12

--- CheapCar/src/com/kh/semi/member/controller/AjaxIdCheckCotroller.java
+++ CheapCar/src/com/kh/semi/member/controller/AjaxIdCheckCotroller.java
@@ -12,7 +12,7 @@ import com.kh.semi.member.model.service.MemberService;
 /**
  * Servlet implementation class AjaxIdCheckCotroller
  */
-@WebServlet("/idCheck")
+@WebServlet("/idCheck.do")
 public class AjaxIdCheckCotroller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
