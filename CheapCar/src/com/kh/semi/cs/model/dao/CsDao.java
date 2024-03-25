201ef2a888c98b840d3b1df01ab72a28df028e11
Beenzinoooooooooo <306kjung@naver.com>
2024-03-25 11:49:54 +0900

    1
    1

--- CheapCar/src/com/kh/semi/cs/model/dao/CsDao.java
+++ CheapCar/src/com/kh/semi/cs/model/dao/CsDao.java
@@ -1,5 +1,53 @@
 package com.kh.semi.cs.model.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.kh.semi.common.JDBCTemplate.*;
+
 public class CsDao {
+	
+	private Properties prop = new Properties();
+	
+	public CsDao(){
+		
+		String filePath = CsDao.class.getResource("/sql/cs/cs-mapper.xml").getPath();
+		
+		try {
+			prop.loadFromXML(new FileInputStream(filePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int selectCsListCount(Connection conn) {
+		
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("selectCsListCount");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			rset = pstmt.executeQuery();
+			
+			rset.next();
+			
+			result = rset.getInt("count(*)");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return result;
+	}
 
 }
--- CheapCar/src/com/kh/semi/cs/model/service/CsService.java
+++ CheapCar/src/com/kh/semi/cs/model/service/CsService.java
@@ -1,5 +1,22 @@
 package com.kh.semi.cs.model.service;
 
+import static com.kh.semi.common.JDBCTemplate.*;
+
+import java.sql.Connection;
+
+import com.kh.semi.cs.model.dao.CsDao;
+
 public class CsService {
+	
+	public int selectCsListCount() {
+		
+		Connection conn = getConnection();
+		
+		int result = new CsDao().selectCsListCount(conn);
+		
+		close(conn);
+		
+		return result;
+	}
 
 }
