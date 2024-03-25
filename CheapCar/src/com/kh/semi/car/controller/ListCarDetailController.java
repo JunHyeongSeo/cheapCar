2ef97b93a9455b80672d612c0df1b8499829dbdc
PhilnoS2 <164119126+PhilnoS2@users.noreply.github.com>
2024-03-25 10:22:34 +0900

    현빈 - 차 디테일 작업중

--- CheapCar/src/com/kh/semi/car/controller/ListCarDetailController.java
+++ CheapCar/src/com/kh/semi/car/controller/ListCarDetailController.java
@@ -2,12 +2,15 @@ package com.kh.semi.car.controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.semi.car.model.service.CarService;
+import com.kh.semi.car.model.vo.Car;
+import com.kh.semi.car.model.vo.Option;
 
 /**
  * Servlet implementation class ListCarDetailController
@@ -29,9 +32,14 @@ public class ListCarDetailController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		int managementNo = Integer.parseInt(request.getParameter("carNo"));
 		
+		Car car = new CarService().selectDetailCar(managementNo);
 		
+		Option option = new CarService().selectDetailOption(managementNo);
 		
+		request.setAttribute("Car", car);
+		request.setAttribute("Option", option);
 		
 		request.getRequestDispatcher("views/car/carDetail.jsp").forward(request, response);
 	
