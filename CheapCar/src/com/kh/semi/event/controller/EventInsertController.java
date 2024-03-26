7527bc202047aab6f618c3d911d29c3b147ddf2a
dongja86 <becksmania7@gmail.com>
2024-03-26 16:56:18 +0900

    Update EventInsertController.java

--- CheapCar/src/com/kh/semi/event/controller/EventInsertController.java
+++ CheapCar/src/com/kh/semi/event/controller/EventInsertController.java
@@ -70,9 +70,20 @@ public class EventInsertController extends HttpServlet {
 					
 					EventPhoto ePhoto = new EventPhoto();
 					ePhoto.setPhotoName(multiRequest.getOriginalFileName(key));
-					ePhoto.setPhotoPath("/resources/event_upfiles");
+					ePhoto.setPhotoPath("resources/event_upfiles");
+					
+					if(i == 1) {
+						// 대표이지미
+						ePhoto.setFileLevel(1);
+						
+					} else {
+						ePhoto.setFileLevel(2);
+					}
+					list.add(ePhoto);
 					
 				}
+				
+				new EventService().insertEventBoard(eBoard, list);
 				
 				
 				
