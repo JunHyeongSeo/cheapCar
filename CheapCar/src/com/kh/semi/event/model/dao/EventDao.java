d0f7cfb9d572d931af49ba8ab4c26c5b5a4bb5a8
dongja86 <becksmania7@gmail.com>
2024-03-26 17:13:09 +0900

    ㄷㅇ

--- CheapCar/src/com/kh/semi/event/model/dao/EventDao.java
+++ CheapCar/src/com/kh/semi/event/model/dao/EventDao.java
@@ -1,5 +1,48 @@
 package com.kh.semi.event.model.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Properties;
+
+import com.kh.semi.event.model.vo.EventBoard;
+
 public class EventDao {
 
-}
+	private Properties prop = new Properties();
+	
+	public EventDao() {
+		
+		String filePath = EventDao.class.getResource("/sql/event/event-mapper.xml").getPath();
+	
+		//System.out.println(filePath);
+		
+		try {
+			prop.loadFromXML(new FileInputStream(filePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}//	
+	
+	public int insertEventBoard(Connection conn, EventBoard eBoard) {
+		
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql = prop.getProperty("insertEventBoard");
+		
+		
+		
+		
+		
+		
+		
+		return result;
+	}//
+	
+	
+	
+	
+	
+}//
