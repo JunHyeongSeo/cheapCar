7af7a197e199b635ed985e736dad84728a897492
Beenzinoooooooooo <306kjung@naver.com>
2024-03-27 23:36:35 +0900

    1
    1

--- CheapCar/src/com/kh/semi/admin/admin_member/AjaxSelectMemsController.java
+++ CheapCar/src/com/kh/semi/admin/admin_member/AjaxSelectMemsController.java
@@ -1,6 +1,6 @@
-package com.kh.semi.admin.admin_member;
-
-import java.io.IOException;
+	package com.kh.semi.admin.admin_member;
+	
+	import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -10,60 +10,61 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.kh.semi.common.model.vo.PageInfo;
 import com.kh.semi.member.model.service.MemberService;
 import com.kh.semi.member.model.vo.Member;
-
-/**
- * Servlet implementation class AdminSelectMemberController
- */
-@WebServlet("/adminSMS")
-public class AjaxSelectMemsController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AjaxSelectMemsController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
+	
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Servlet implementation class AdminSelectMemberController
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String searchId = request.getParameter("searchId");
-		
-		int listCount = new MemberService().selectListCount();
-		int currentPage = Integer.parseInt(request.getParameter("num"));
-		int pageLimit = 10;
-		int boardLimit = 5;
-		int maxPage =(int)Math.ceil((double)listCount / boardLimit);
-		int startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
-		int endPage = startPage + pageLimit - 1;
-		if(endPage > maxPage) {
-			endPage = maxPage;
+	@WebServlet("/adminSMS")
+	public class AjaxSelectMemsController extends HttpServlet {
+		private static final long serialVersionUID = 1L;
+	       
+	    /**
+	     * @see HttpServlet#HttpServlet()
+	     */
+	    public AjaxSelectMemsController() {
+	        super();
+	        // TODO Auto-generated constructor stub
+	    }
+	
+		/**
+		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+			
+			String searchId = request.getParameter("searchId");
+			
+			int listCount = new MemberService().selectListCount(searchId);
+			int currentPage = Integer.parseInt(request.getParameter("currentPage"));
+			int pageLimit = 10;
+			int boardLimit = 5;
+			int maxPage =(int)Math.ceil((double)listCount / boardLimit);
+			int startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
+			int endPage = startPage + pageLimit - 1;
+			if(endPage > maxPage) {
+				endPage = maxPage;
+			}
+			
+			PageInfo pi = new PageInfo(listCount, currentPage, pageLimit, boardLimit, maxPage, startPage, endPage);
+			
+			ArrayList<Member> list = new MemberService().asmcs(pi, searchId);
+			
+			response.setContentType("application/json; charset=UTF-8");
+			
+			new Gson().toJson(list, response.getWriter());
+			
 		}
-		
-		PageInfo pi = new PageInfo(listCount, currentPage, pageLimit, boardLimit, maxPage, startPage, endPage);
-		
-		ArrayList<Member> list = new MemberService().asmcs(pi, searchId);
-		
-		response.setContentType("application/json; charset=UTF-8");
-		
-		new Gson().toJson(list, response.getWriter());
-		System.out.println(pi);
-		
+	
+		/**
+		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+			doGet(request, response);
+		}
+	
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
-}
