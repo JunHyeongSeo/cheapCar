fc60ffaec078f6367ca058d6378494b67948326a
dongja86 <becksmania7@gmail.com>
2024-03-28 16:22:46 +0900

    ㄷㅇ

--- CheapCar/src/com/kh/semi/event/controller/EventUpdateFormController.java
+++ CheapCar/src/com/kh/semi/event/controller/EventUpdateFormController.java
@@ -1,11 +1,14 @@
 package com.kh.semi.event.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.kh.semi.event.model.service.EventService;
 
 /**
  * Servlet implementation class EventUpdateFormController
@@ -26,8 +29,21 @@ public class EventUpdateFormController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		int eventNo = Integer.parseInt(request.getParameter("eventNo"));
+		
+		new EventService().selectEvent(eventNo);
+		new EventService().selectEventPhoto(eventNo);
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 	/**
