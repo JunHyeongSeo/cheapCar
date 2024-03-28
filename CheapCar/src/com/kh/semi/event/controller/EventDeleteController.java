222dfa06a95769f1a82e2713a24cce7389c7ae1a
dongja86 <becksmania7@gmail.com>
2024-03-28 15:05:38 +0900

    ㄷㅇ

---
+++ CheapCar/src/com/kh/semi/event/controller/EventDeleteController.java
@@ -0,0 +1,55 @@
+package com.kh.semi.event.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EventDeleteController
+ */
+@WebServlet("/delete.event")
+public class EventDeleteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EventDeleteController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		int eventNo = Integer.parseInt(request.getParameter("eventNo"));
+		
+		new EventService().deleteEvent(eventNo);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- CheapCar/src/com/kh/semi/event/model/service/EventService.java
+++ CheapCar/src/com/kh/semi/event/model/service/EventService.java
@@ -90,6 +90,10 @@ public class EventService {
 	
 	
 	
+	
+	
+	
+	
 		
 		
 		
