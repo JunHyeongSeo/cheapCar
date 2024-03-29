29f5cfdbbcab2c69232b962f0535ca384bff062b
dongja86 <becksmania7@gmail.com>
2024-03-29 16:40:47 +0900

    ㄷㅇ

--- CheapCar/src/com/kh/semi/event/controller/EventUpdateController.java
+++ CheapCar/src/com/kh/semi/event/controller/EventUpdateController.java
@@ -1,6 +1,7 @@
 package com.kh.semi.event.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
+import com.kh.semi.common.MyFileRenamePolicy;
+import com.oreilly.servlet.MultipartRequest;
 
 /**
  * Servlet implementation class EventUpdateController
@@ -36,6 +40,21 @@ public class EventUpdateController extends HttpServlet {
 			int maxSize = 1024 * 1024 * 10;
 			
 			String savePath = request.getServletContext().getRealPath("sources/event/event_upfiles");
+			
+			MultipartRequest multiRequest = new MultipartRequest(request, savePath, maxSize, "UTF-8", new MyFileRenamePolicy());
+			
+			//----------------------
+			
+			int eventNo = Integer.parseInt(multiRequest.getParameter("eventNo"));
+			String eventTitle = multiRequest.getParameter("title");
+			String eventConetent = multiRequest.getParameter("content");
+			
+			
+			
+			
+			
+			
+			
 			
 			
 		}
